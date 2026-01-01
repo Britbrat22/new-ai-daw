5a6fc85bc5f25709a123763d93d9bdc8effef78c
Britbrat22 <131219149+Britbrat22@users.noreply.github.com>
2025-12-31 22:47:20 -1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/new-ai-daw/', // must match your repo name exactly
-  plugins: [react()],
-})
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   base: "/new-ai-daw/",
   plugins: [react()],
-});
->>>>>>> 3e6b66cde638a7805e0281caf00008541721a7af
+})
